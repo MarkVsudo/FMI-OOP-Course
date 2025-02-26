7e2791fc981549dadd46b13b6689e42e724f0875
Mark Veskov <mark.veskov@abv.bg>
2025-02-26 23:42:11 +0200

    Task 4 Week 2 OOP

---
+++ Practicum/Week_02/task4.cpp
@@ -0,0 +1,82 @@
+// Създайте структура, която представлява фигурата триъгълник. Също така, реализирайте следните функции:
+
+// четене от стандартния вход
+// принтиране на стандартния изход
+// пресмятане на обиколка
+// пресмятане на лице
+// връщане на типа на триъгълника спрямо страните му (равнобедрен, равностранен, разностранен)
+// връщане на типа на триъгълника спрямо ъглите му (остроъгълен, правоъгълен, тъпоъгълен)
+
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+struct Triangle
+{
+  double a, b, c;
+};
+
+void readTriangleData(Triangle &tri)
+{
+  cout << "Enter sides of triangle (a, b, c): ";
+  cin >> tri.a >> tri.b >> tri.c;
+}
+
+void printTriangleData(const Triangle &tri)
+{
+  cout << "Triangle sides: a = " << tri.a << " b = " << tri.b << " c = " << tri.c << endl;
+}
+
+double calcTriPerimeter(const Triangle &tri)
+{
+  return tri.a + tri.b + tri.c;
+}
+
+double calcTriArea(const Triangle &tri)
+{
+  double s = calcTriPerimeter(tri) / 2;
+  return sqrt(s * (s - tri.a) * (s - tri.b) * (s - tri.c));
+}
+
+void getTriangleTypeBySides(const Triangle &tri)
+{
+  if (tri.a == tri.b && tri.b == tri.c)
+    cout << "Ravnostranen" << endl;
+  else if (tri.a == tri.b || tri.a == tri.c || tri.b == tri.c)
+    cout << "Ravnobedren" << endl;
+  else
+    cout << "Raznostranen" << endl;
+}
+
+void getTriangleTypeByAngles(const Triangle &tri)
+{
+  double a2 = tri.a * tri.a,
+         b2 = tri.b * tri.b,
+         c2 = tri.c * tri.c;
+
+  if (a2 + b2 == c2 || a2 + c2 == b2 || b2 + c2 == a2)
+    cout << "Pravougulen" << endl;
+  else if (a2 + b2 > c2 && a2 + c2 > b2 && b2 + c2 > a2)
+    cout << "Ostrougulen" << endl;
+  else
+    cout << "Tupougulen" << endl;
+}
+
+int main()
+{
+  Triangle tri;
+
+  readTriangleData(tri);
+  printTriangleData(tri);
+
+  cout << "Perimeter: " << calcTriPerimeter(tri) << endl;
+  cout << "Area: " << calcTriArea(tri) << endl;
+
+  getTriangleTypeBySides(tri);
+  getTriangleTypeByAngles(tri);
+
+  return 0;
+}
