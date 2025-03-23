10f6f4a4a0528684e4d6ef53c3990fb1007516d5
Mark Veskov <mark.veskov@abv.bg>
2025-03-23 11:36:47 +0200

    Task 1 | Week 4 | OOP Practicum

--- Practicum/Week_04/Task_1/task1.cpp
+++ Practicum/Week_04/Task_1/task1.cpp
@@ -5,6 +5,18 @@
 
 int main()
 {
+  std::ifstream inputFile("data.txt", std::ios::binary | std::ios::ate);
+
+  if (!inputFile)
+  {
+    std::cerr << "Error opening file for reading!" << std::endl;
+    return 1;
+  }
+
+  size_t fileSize = inputFile.tellg();
+  inputFile.close();
+
+  std::cout << "File size: " << fileSize << " bytes" << std::endl;
 
   return 0;
 }
