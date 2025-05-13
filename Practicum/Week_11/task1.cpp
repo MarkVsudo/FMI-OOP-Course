a34a47f14ea3e44c5768f4edfc99a25686bddec2
Mark Veskov <mark.veskov@abv.bg>
2025-05-14 00:04:12 +0300

    Task 1 | Week 11 | OOP Practicum

--- Practicum/Week_11/task1.cpp
+++ Practicum/Week_11/task1.cpp
@@ -7,8 +7,8 @@ class Vehicle
 public:
   Vehicle() : color(nullptr), make(nullptr), year(2000), passengers(1), speed(10) {}
 
-  Vehicle(char *color,
-          char *make,
+  Vehicle(const char *color,
+          const char *make,
           unsigned short year,
           unsigned short passengers,
           unsigned short speed)
@@ -20,7 +20,7 @@ public:
     setSpeed(speed);
   }
 
-  ~Vehicle()
+  virtual ~Vehicle()
   {
     delete[] color;
     delete[] make;
@@ -111,6 +111,55 @@ private:
 class Bicycle : public Vehicle
 {
 public:
+  Bicycle()
+      : Vehicle(), gears(1), lights(false), ring(false) {}
+
+  Bicycle(const char *color, const char *make,
+          unsigned short year, unsigned short passengers, unsigned short speed,
+          unsigned short gears, bool lights, bool ring)
+      : Vehicle(color, make, year, passengers, speed)
+  {
+    setGears(gears);
+    this->lights = lights;
+    this->ring = ring;
+  }
+
+  Bicycle(const Bicycle &other)
+      : Vehicle(other)
+  {
+    this->gears = other.gears;
+    this->lights = other.lights;
+    this->ring = other.ring;
+  }
+
+  Bicycle &operator=(const Bicycle &other)
+  {
+    if (this != &other)
+    {
+      Vehicle::operator=(other);
+      this->gears = other.gears;
+      this->lights = other.lights;
+      this->ring = other.ring;
+    }
+    return *this;
+  }
+
+  ~Bicycle() override = default;
+
+  void setGears(unsigned short gears)
+  {
+    if (gears < 1)
+      throw std::invalid_argument("Bicycle must have at least 1 gear");
+    this->gears = gears;
+  }
+
+  void setLights(bool hasLights) { this->lights = hasLights; }
+  void setRing(bool hasRing) { this->ring = hasRing; }
+
+  unsigned short getGears() const { return gears; }
+  bool hasLights() const { return lights; }
+  bool hasRing() const { return ring; }
+
 private:
   unsigned short gears;
   bool lights;
@@ -120,6 +169,61 @@ private:
 class Car : public Vehicle
 {
 public:
+  Car() : Vehicle(), model(nullptr), doors(0), power(0) {}
+
+  Car(const char *color, const char *make, unsigned short year, unsigned short passengers, unsigned short speed, const char *model, unsigned short doors, unsigned short power) : Vehicle(color, make, year, passengers, speed)
+  {
+    setModel(model);
+    this->doors = doors;
+    this->power = power;
+  }
+
+  ~Car() override
+  {
+    delete[] model;
+  };
+
+  Car(const Car &other) : Vehicle(other)
+  {
+    setModel(other.model);
+    this->doors = other.doors;
+    this->power = other.power;
+  }
+
+  Car &operator=(const Car &other)
+  {
+    if (this != &other)
+    {
+      Vehicle::operator=(other);
+      setModel(other.model);
+      this->doors = other.doors;
+      this->power = other.power;
+    }
+    return *this;
+  }
+
+  void setModel(const char *model)
+  {
+    if (this->model)
+    {
+      delete[] this->model;
+    }
+    size_t modelSize = strlen(model);
+    this->model = new char[modelSize + 1];
+    strcpy(this->model, model);
+    this->model[modelSize] = '\0';
+  }
+
+  void setDoors(unsigned short doors)
+  {
+    this->doors = doors;
+  }
+
+  void setPower(unsigned short power)
+  {
+    this->power = power;
+  }
+
 private:
   char *model;
   unsigned short doors;
